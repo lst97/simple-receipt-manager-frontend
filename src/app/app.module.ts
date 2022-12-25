8116ffc0675ecc38d56089ce371742731b9903ae
lst97 <laisiotou1997@gmail.com>
2022-12-25 20:25:00 +1100

    fix: nested path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './common/header/header/header.component';
+import { HeaderComponent } from './common/header/header.component';
 
 
 @NgModule({
