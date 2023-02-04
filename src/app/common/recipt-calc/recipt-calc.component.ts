f51dc33af4546cfbc220f592afb402e87acf1a1f
lst97 <laisiotou1997@gmail.com>
2023-02-04 12:58:42 +1100

    build: apply layout

--- src/app/common/recipt-calc/recipt-calc.component.ts
+++ src/app/common/recipt-calc/recipt-calc.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormGroup, FormControl } from '@angular/forms';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+}
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079 },
+  { position: 2, name: 'Helium', weight: 4.0026 },
+  { position: 3, name: 'Lithium', weight: 6.94 },
+  { position: 4, name: 'Beryllium', weight: 9.0122 },
+  { position: 5, name: 'Boron', weight: 10.811 },
+  { position: 6, name: 'Carbon', weight: 12.0107 },
+];
 
 @Component({
   selector: 'app-recipt-calc',
   templateUrl: './recipt-calc.component.html',
-  styleUrls: ['./recipt-calc.component.scss']
+  styleUrls: ['./recipt-calc.component.scss'],
 })
-export class ReciptCalcComponent {
+export class ReciptCalcComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  displayedColumns: string[] = ['position', 'name', 'weight'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  constructor() {}
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
